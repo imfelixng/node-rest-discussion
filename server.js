da70e242e5a86831621a5a34e394d1b3336eb10b
Nguyen Quang An <ngquangan@gmail.com>
2019-02-09 21:10:32 +0700

    Connected mongodb

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
-require('dotenv').config({ path: '.env' });
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => (
   res.status(200).json({
     message: 'It works',
   })
 ));
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+}, (err) => {
+  if (!err) {
+    console.log('MongoDB connected');
+  } else {
+    console.log(err);
+  }
+});
 
 const port = process.env.PORT || 8000;
 
