ecff1d8f6aa3b4b52268ddfebf42ce8562017630
Nguyen Quang An <ngquangan@gmail.com>
2019-02-09 22:25:27 +0700

    Add Account Schema

---
+++ models/Account.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const AccountSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  role: {
+    type: Number,
+    required: true,
+  },
+  isConfirmed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = mongoose.model('Account', AccountSchema);
